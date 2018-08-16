29170504903ec94826c007811d07fb5022c33504
baobaojianqiang <16230091@qq.com>
2018-08-16 23:34:25 +0800

    suport 26+: AMN gDefault

--- app/src/main/java/jianqiang/com/hook6/HookHelper.java
+++ app/src/main/java/jianqiang/com/hook6/HookHelper.java
@@ -11,8 +11,15 @@ public final class HookHelper {
 
     public static void hookActivityManager() {
         try {
-            //获取AMN的gDefault单例gDefault，gDefault是静态的
-            Object gDefault = RefInvoke.getStaticFieldObject("android.app.ActivityManagerNative", "gDefault");
+            //获取AMN的gDefault单例gDefault，gDefault是final静态的
+            Object gDefault = null;
+            if (android.os.Build.VERSION.SDK_INT <= 25) {
+                //获取AMN的gDefault单例gDefault，gDefault是静态的
+                gDefault = RefInvoke.getStaticFieldObject("android.app.ActivityManagerNative", "gDefault");
+            } else {
+                //获取ActivityManager的单例IActivityManagerSingleton，他其实就是之前的gDefault
+                gDefault = RefInvoke.getStaticFieldObject("android.app.ActivityManager", "IActivityManagerSingleton");
+            }
 
             // gDefault是一个 android.util.Singleton对象; 我们取出这个单例里面的mInstance字段，IActivityManager类型
             Object rawIActivityManager = RefInvoke.getFieldObject(
